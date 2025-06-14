5c0261653f697bfc0f9060e09208ddbe27c465b2
karanveer singh gill <kgill32@academic.rrc.ca>
2025-06-13 23:23:24 -0500

    Adding interest in account balances

--- interest_update.py
+++ interest_update.py
@@ -14,4 +14,23 @@ with open("account_balances.txt", "r") as input_file:
 
 pprint.pprint(customer_account)
 
+# Processing of account
+for account_number in customer_account:
+    balance = customer_account[account_number]
+
+    if balance >= 5000:
+        interest = 0.05
+    elif balance >= 1000:
+        interest = 0.025
+    elif balance > 0:
+        interest = 0.01
+    elif balance < 0:
+        interest = 0.1
+    else:
+        interest = 0
+ 
+        # Crediting interest in account balance
+    customer_account[account_number] += balance * interest / 12
+    pprint.pprint(customer_account) 
+
 
